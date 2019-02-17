ee119fbd8892f2ba68171545c9ef7803ec89bbbc
Dennis Gill <dengill@ucdavis.edu>
2019-02-17 01:55:34 -0800

    Fixed 2 instances of Second BUG
    	inefficient use of keySet iterator instead of entrySet iterator

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import edu.studyup.entity.Event;
 import edu.studyup.entity.Student;
@@ -34,8 +35,13 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> activeEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
+//      BUG : inefficient use of keySet iterator instead of entrySet iterator
+//		for (Integer key : eventData.keySet()) {
+//			Event ithEvent= eventData.get(key);
+//			activeEvents.add(ithEvent);
+		
+		for (Map.Entry<Integer, Event> entry: eventData.entrySet()) {
+			Event ithEvent= entry.getValue();
 			activeEvents.add(ithEvent);
 		}
 		return activeEvents;
@@ -46,8 +52,11 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> pastEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
+//      BUG : inefficient use of keySet iterator instead of entrySet iterator
+//		for (Integer key : eventData.keySet()) {
+//			Event ithEvent= eventData.get(key);
+		for (Map.Entry<Integer, Event> entry: eventData.entrySet()) {
+			Event ithEvent= entry.getValue();
 			// Checks if an event date is before today, if yes, then add to the past event list.
 			if(ithEvent.getDate().before(new Date())) {
 				pastEvents.add(ithEvent);
